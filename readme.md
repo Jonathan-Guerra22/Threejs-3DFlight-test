3f4e2503536ec6c6a641a65b129f5cd7812d0c61
JHØNY <92238626+Jonathan-Guerra22@users.noreply.github.com>
2023-01-03 19:05:48 -0500

    download project from the original repo and make minor fixes

---
+++ readme.md
@@ -0,0 +1,112 @@
+# [flightmap.live](https://flightmap.live/)
+
+https://user-images.githubusercontent.com/57844356/174462290-6cc55406-075e-42ef-a245-b7ba17ada1e4.mov
+
+## About
+A realtime visualization of the last 48 hours of airplane departures on a 3D interactive globe. This project was made using WebGL (ThreeJS), Javascript, HTML/CSS, airport data, and live airplane data via an API.
+
+* Airplane information is obtained via ADS-B and Mode S data collected by research institutions and local users on the OpenSky Network (https://opensky-network.org/)
+* Airport data (ICAO, City, Country, Coordinates) was parsed from the Global Airport Database (https://www.partow.net/miscellaneous/airportdatabase/)
+
+## Data Collection 
+For complete airport data, I downloaded the Global Airport Database and wrote a little Python script to parse the data, convert it onto a CSV, and get rid of any data that would cause issues.
+
+``` python
+import pandas as pd
+
+col_names = ['ICAO', 'name', 'city', 'country', 'lat', 'long']
+# Load in data with only the airport's ICAO, city, country, latitude and longitude columns
+df = pd.read_csv("GlobalAirportDatabase.txt", sep = ":", header=None, usecols = [0, 2, 3, 4, 14, 15], names=col_names)
+
+# Drop all rows with no coordinates
+df = df[df['lat'] !=0]
+
+# Convert to CSV
+df.to_csv('aiport_coords.csv', index=False)
+
+``` 
+This CSV is then simply converted into a JavaScript hashmap as shown below (airport_data.js)
+``` js
+  },
+  "KSFO": {
+    "name": "SAN FRANCISCO INTERNATIONAL",
+    "city": "SAN FRANCISCO",
+    "country": "USA",
+    "lat": 37.619,
+    "long": -122.375
+  },
+```
+
+The next step in data collection was finding a live airplane API (preferably free) that had the information I needed, which was departure airport, arrival airport, ICAO code, and to be able to filter flights by time frame. For this I chose the OpenSky Network Live API (https://openskynetwork.github.io/opensky-api/)
+
+## Code Walkthrough
+* The functions below are seen in /src/globe.js
+
+Say the user enters KSFO (San Fransisco International) and clicks "View Flights"...
+
+The first step is calling the getTimes() function for finding the right time frame for the API call. This function simply returns the current time, and the current time - 48 hrs, all in Unix time (seconds since epoch)
+
+``` js
+function getTimes(){
+    currentTime = Math.floor(Date.now() / 1000)
+    prevTime = currentTime - (2 * 24 * 60 * 60)
+    return [String(prevTime), String(currentTime)]
+}
+
+```
+
+The next step is getting San Fransico International's JSON departure data from the OpenSky network in that time frame (getData(ICAO='KSFO')). The request to OpenSky only returns the ICAO code for the arrival destination of each flight. That's when we can search through the data created in our data collection step to find the corresponding coordinates, city and country. The data for each flight is stored in an array called routes. 
+
+Now the exciting part, drawing the routes. The first step is to rotate the globe to the departure, KSFO. 
+
+``` js
+// 600ms transition to new view point
+function rotateGlobe(lat=37.619, long=-122.375){
+  globe.pointOfView({lat: lat, lng:long, altitude: 2.2}, 600)
+}
+
+```
+Second, we draw the routes on our globe, along with a label for the departure airport
+``` js
+const colors = [['#CCF381', '#4831D4'], ['#9400D3','#FFAA33'], ['#89ABE3FF', '#EA738DFF']]
+function setRoutes(routes, ICAO){
+    newLabel = [{lat: DATA[ICAO].lat, lng: DATA[ICAO].long, label: DATA[ICAO].name + " ("+ ICAO + ")"}]
+    globe.arcColor(colors[Math.floor(Math.random() * colors.length)])
+    globe.labelsData(newLabel)
+    globe.arcsData(routes);
+}
+```
+Last, we attach a label to each route showing the arrival airport's information
+``` js
+function showRoute(arc, prevarc){
+    if (arc != null){
+        globe.arcLabel(["→ " + arc.arrCity + ", " + arc.arrCountry + " (" + arc.arrICAO + ")"])
+    }
+}
+```
+
+And that's it! There are more minutiae such as styling the globe/wesbite, parsing the data, offsets, attaching event listeners etc., but that can all be explored in the /src/ folder
+
+
+
+
+
+
+
+## Running on your local network
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+``` bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+```
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ src/globe.js
@@ -0,0 +1,129 @@
+// Shifting globe to the right 
+const w = window.innerWidth;
+const shiftFactor = 0.35;
+const shiftAmmount = shiftFactor * w;
+
+// Flags to set starting parameters
+const startAirport = 'CYYZ'
+var addInitialOffset = true
+var startColor = true
+
+const colors = [['#CCF381', '#4831D4'], ['#D198C5FF' ,'#E0C568FF' ],['#89ABE3FF', '#EA738DFF'], ['#FDF5DF', '#F92C85'], 
+                ['#2BAE66FF', '#FCF6F5FF'], ['#9400D3','#FFAA33']]
+const globe = Globe()
+    (document.getElementById('globeViz'))
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+    .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
+    .width(w + shiftAmmount)
+    .pointOfView({ lat: 29.197123, lng: -46.185931, altitude: 2.2})
+    .arcsTransitionDuration(500)
+    .arcDashLength(0.15)
+    .arcDashGap(0.01)
+    .arcDashInitialGap(() => Math.random())
+    .arcDashAnimateTime(6000)
+    .onArcHover(showRoute)
+    .labelText('label')
+    .labelSize(1.5)
+    .labelDotRadius(0.4)
+    .labelAltitude(0.1)
+    .labelDotOrientation(() => 'right')
+    .labelColor(() => 'whitesmoke')
+    .labelsTransitionDuration(0)
+
+getData(callback = setRoutes, ICAO=startAirport);
+
+
+// Function to get currentTime and currenTime - 48hrs in Unix time
+// ***Data is backtracked by one month, due to issues with OpenSky Network
+// ***Will go back to live data as soon as possible
+function getTimes(){
+    currentTime = Math.floor(Date.now() / 1000) - 2630000
+    prevTime = currentTime - (2 * 24 * 60 * 60) 
+    return [String(prevTime), String(currentTime)]
+}
+
+// Gets JSON airport departure data from OpenSky 
+function getData(callback, ICAO){
+    const depLat = DATA[ICAO].lat
+    const depLng = DATA[ICAO].long 
+    rotateGlobe(depLat, depLng)
+    times = getTimes()
+    var routes = []
+    const url = "https://opensky-network.org/api/flights/departure?airport=" + ICAO + "&begin=" + times[0] + "&end=" + times[1]
+    var request = new XMLHttpRequest()
+    request.open('GET', url, true)
+    request.onload = function () {
+        var data = JSON.parse(this.response)
+        var seenFlights = new Set()
+        if (request.status >= 200 && request.status < 400) {
+            data.forEach(flight => {
+                const arrivalAirport = flight.estArrivalAirport
+                if (arrivalAirport!= null && seenFlights.has(arrivalAirport) != true && arrivalAirport != ICAO && DATA[arrivalAirport] != null) {
+                    seenFlights.add(arrivalAirport)
+                    arrivalAirportData = DATA[flight.estArrivalAirport]
+                    routes.push({startLat: depLat, startLng: depLng, endLat: arrivalAirportData.lat, endLng: arrivalAirportData.long, 
+                                arrICAO: arrivalAirport, arrCity: arrivalAirportData.city, arrCountry: arrivalAirportData.country })
+                }
+            })
+        } else {
+        console.log('error')
+        }
+        callback(routes, ICAO)
+    }
+    request.send()
+}
+
+
+// Smooth spin of the globe to a new airport
+function rotateGlobe(lat, long){
+    if (addInitialOffset == true) {
+        globe.pointOfView({lat: 35, lng:-60, altitude: 2.2}, 600)
+        addInitialOffset = false
+    }
+    else {
+        globe.pointOfView({lat: lat, lng:long, altitude: 2.2}, 600)
+    }
+}
+
+// Populates the globe with new airport data
+function setRoutes(routes, ICAO){
+    newLabel = [{lat: DATA[ICAO].lat, lng: DATA[ICAO].long, label: DATA[ICAO].name + " ("+ ICAO + ")"}]
+    if (startColor == true){
+        globe.arcColor(colors[0])
+        startColor = false
+    }
+    else{
+        globe.arcColor(colors[Math.floor(Math.random() * colors.length)])
+    }
+    globe.labelsData(newLabel)
+    globe.arcsData(routes);
+}
+
+// Changing Airports when user clicks button
+function btnSubmit(CIUDAD) {
+    console.log(Date());
+    console.log(CIUDAD);
+    let checkboxes = document.querySelectorAll('input[id="'+CIUDAD+'"]:checked');
+    if (checkboxes.length !== 0) getData(callback = setRoutes, ICAO=checkboxes[0].value);
+}
+// Event listener for button click
+var bogotaId = document.getElementById('BOGOTA')
+bogotaId.addEventListener('click', ()=>btnSubmit('BOGOTA'))
+
+var madridId = document.getElementById('MADRID')
+madridId.addEventListener('click', ()=>btnSubmit('MADRID'))
+
+var losAngelesId = document.getElementById('LOS-ANGELES')
+losAngelesId.addEventListener('click', ()=>btnSubmit('LOS-ANGELES'))
+
+function showRoute(arc, prevarc){
+    console.log(arc);
+    if (arc != null){
+        globe.arcLabel(["→ " + arc.arrCity + ", " + arc.arrCountry + " (" + arc.arrICAO + ")"])
+    }
+}
+
+
+
+
